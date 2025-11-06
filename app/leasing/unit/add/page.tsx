ef2f94bd4780b48f154846371e7fc14cdb63cc6b
jeffcabu31 <jeffcabu@gmail.com>
2025-11-06 22:35:21 +0000

    pre-departure

---
+++ app/leasing/unit/add/page.tsx
@@ -0,0 +1,9 @@
+import UnitForm from "@/app/components/own/forms/unit-form";
+
+export default function Page() {
+  return (
+    <div className="container max-w-3xl mx-auto">
+      <UnitForm />
+    </div>
+  );
+}
